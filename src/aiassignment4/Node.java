8c47e7a41dec4ff8da594673de5bdacae2c0fed2
Maeda Hanafi <my_pink_han@yahoo.com>
2015-04-24 19:47:40 +0400

    first

---
+++ src/aiassignment4/Node.java
@@ -0,0 +1,45 @@
+
+package aiassignment4;
+
+/**
+ *
+ * Node class
+ * Maeda Hanafi
+ */
+public class Node {
+    private char character;
+    private Node parent;
+    private Node child1;
+    private Node child2;
+    int index;
+    public Node(char operator, Node child1, Node child2){
+        this.character = operator;
+        this.child1 = child1;
+        this.child2 = child2;
+    }
+
+    public void setParent(Node parent){
+        this.parent = parent;
+    }
+    public char getData(){
+        return character;
+    }
+    public Node getChild1(){
+        return child1;
+    }
+    public Node getChild2(){
+        return child2;
+    }
+    public void setChild1(Node child1){
+        this.child1 = child1;
+    }
+    public void setChild2(Node child2){
+        this.child2 = child2;
+    }
+    public boolean isTerminal(){
+        if(this.character == '+' || this.character == '-' || this.character == '*' || this.character == '/' || this.character == 's' ){
+            return false;
+        }
+        return true;
+    }
+}
